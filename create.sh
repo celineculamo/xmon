0a40de6921220ede00d0cf16a639bef754e2011d
celineculamo <celine.culamo@gmail.com>
2024-10-20 19:40:34 +0200

    Create create.sh

---
+++ create.sh
@@ -0,0 +1,11 @@
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig/
+mkdir build
+cd build
+cmake ..
+make
+
