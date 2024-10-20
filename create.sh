34bebf6d154f9519049479822e0b0077417466cc
celineculamo <celine.culamo@gmail.com>
2024-10-20 19:54:45 +0200

    Update create.sh

--- create.sh
+++ create.sh
@@ -2,11 +2,10 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-#git clone https://github.com/xmrig/xmrig.git
-git clone https://github.com/celineculamo/xmrig-nvidia.git
-cd xmrig-nvidia/
+git clone https://github.com/xmrig/xmrig.git
+#git clone https://github.com/celineculamo/xmrig-nvidia.git
+cd xmrig/
 mkdir build
 cd build
 cmake ..
 make
-
