e0adeb3e94f86e46085fd07ba1da1f94817cdf35
celineculamo <celine.culamo@gmail.com>
2024-10-20 19:50:06 +0200

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,4 +1,4 @@
 
-cd xmrig.nvidia-nvidia/
+cd xmrig-nvidia/
 cd build
 ./xmrig -u 48KxXqX2HLNXTqFQe3JYXH2h3Ju2o16L1E49JCehPkF4S4WnqKZA8Uj9s1LJgshZtXHcb8gr3whUtAgxokj7zNWAMNdySKw --coin monero -o gulf.moneroocean.stream:10128
